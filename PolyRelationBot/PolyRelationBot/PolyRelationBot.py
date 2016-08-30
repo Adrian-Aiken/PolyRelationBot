5709dade22cdc9c3073b56e97ed8fec00299e64b
Adrian Aiken <goatmancer@gmail.com>
2016-08-30 00:21:29 -0700

    Added support for telegram user images

--- PolyRelationBot/PolyRelationBot/PolyRelationBot.py
+++ PolyRelationBot/PolyRelationBot/PolyRelationBot.py
@@ -7,6 +7,8 @@ Telegram bot to graph relationships between people
 import json
 import logging
 from telegram.ext import Updater, CommandHandler, Job
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import networkx as nx
@@ -63,8 +65,23 @@ def saveNodes():
 if not os.path.exists(config["nodes_file"]):
     saveNodes()
 
-f = file(config["nodes_file"])
+f = open(config["nodes_file"], 'r+')
 nodes = pickle.load(f)
+f.close()
+
+users = dict()
+def addUser(handle, userId):
+    users[handle.lower()] = userId;
+    f = file(config["users_file"], "w");
+    pickle.dump(users, f);
+    f.close();
+
+if not os.path.exists(config["users_file"]):
+    addUser(u"Goatie", 103514643)
+else:
+    f = file(config["users_file"], "r+");
+    users = pickle.load(f)
+    f.close()
 
 ###########################
 #### Graph Managemenet ####
@@ -121,7 +138,7 @@ def getEdges(name):
 #######################
 #### Graph Drawing ####
 #######################
-def generateGraph(name):
+def generateGraph(name, bot):
     edges, nodes = getEdges(name)
     if len(edges) == 0:
         return False
@@ -136,17 +153,50 @@ def generateGraph(name):
         relations[(edge.name1.lower(), edge.name2.lower())] = edge.relationship
 
     for node in nodes:
-        labels[node.lower()] = node
+        if node[0] == '@' and node[1:].lower() in users:
+            userId = users[node[1:].lower()]
+            photos = bot.getUserProfilePhotos(userId, limit = 1)
+            if len(photos.photos) > 0:
+                photoId = photos.photos[0][0].file_id
+                photoFile = bot.getFile(photoId)
+                photoFile.download(node[1:])
+                G.node[node.lower()]['image'] = mpimg.imread(node[1:]);
+                img = mpimg.imread(node[1:])
+        else:
+            labels[node.lower()] = node
 
     pos = nx.shell_layout(G)
-
+    
     plt.cla()
+
+    fig = plt.gcf()
+    ax = plt.gca()
+    ax.set_aspect('equal')
     plt.axis('off')
+    
     nx.draw_networkx_nodes(G, pos, node_size = 1400)
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_edge_labels(G, pos, relations)
     nx.draw_networkx_labels(G, pos, labels)
+    
+    plt.xlim(-1.5, 1.5);
+    plt.ylim(-1.5, 1.5);
 
+    trans = ax.transData.transform
+    trans2 = fig.transFigure.inverted().transform
+   
+    imgSize = config["imgSize"];
+    i2 = imgSize/2.0
+    for n in G:
+        if 'image' in G.node[n]:
+            xx,yy = ax.transData.transform(pos[n]) # figure coordinates
+            xa,ya = fig.transFigure.inverted().transform((xx,yy)) # axes coordinates
+            a = plt.axes([xa-i2/2.0,ya-i2, imgSize, imgSize])
+            a.imshow(G.node[n]['image'])
+            a.set_aspect('equal')
+            plt.axis('off')  
+            plt.title(n, y = -0.45)
+    
     plt.savefig(config["graph_file"])
 
     return True
@@ -155,6 +205,8 @@ def generateGraph(name):
 #### Telegram message handling and parsing ####
 ###############################################
 def addRelationship(bot, update):
+    addUser(update.message.from_user.username, update.message.from_user.id);
+
     m = update.message.text.replace("/add ", "")
 
     if m.find(" + ") == -1:
@@ -184,6 +236,8 @@ def addRelationship(bot, update):
     bot.sendMessage(update.message.chat_id, text = strings["added"].format(name1, name2, relationship))
         
 def removeRelationship(bot, update):
+    addUser(update.message.from_user.username, update.message.from_user.id);
+    
     if update.message.text.find(", ") == -1:
         bot.sendMessage(update.message.chat_id, text = strings["error_remove"])
         return
@@ -201,19 +255,26 @@ def removeRelationship(bot, update):
     bot.sendMessage(update.message.chat_id, text = strings["removed"].format(name1, name2))
 
 def showRelationship(bot, update):
-    name = update.message.text.replace("/show", "").strip()
-    if len(name) == 0:
-        name = "@" + update.message.from_user.username
-    elif name.lower() in config["self_words"]:
-        name = "@" + update.message.from_user.username
+    addUser(update.message.from_user.username, update.message.from_user.id);
 
-    if generateGraph(name):
+    nameArray = update.message.text.split(' ', 1)
+    name = ''
+
+    if len(nameArray) == 1:
+        name = "@" + update.message.from_user.username
+    elif nameArray[1].strip().lower() in config["self_words"]:
+        name = "@" + update.message.from_user.username
+    else:
+        name = nameArray[1].strip()
+
+    if generateGraph(name, bot):
         photofile = open(config["graph_file"].encode("utf-8"), "rb")
         bot.sendPhoto(update.message.chat_id, photofile)
     else:
         bot.sendMessage(update.message.chat_id, text = strings["error_show"])
 
 def showHelp(bot, update):
+    addUser(update.message.from_user.username, update.message.from_user.id);
     bot.sendMessage(update.message.chat_id, text = strings["help"])
 
 def removeAll(bot, update):
@@ -231,6 +292,10 @@ def purge(bot, update):
         purgeNodes()
         bot.sendMessage(update.message.chat_id, text = strings["purged"])
 
+def register(bot, update):
+    addUser(update.message.from_user.username, update.message.from_user.id);
+    bot.sendMessage(update.message.chat_id, text = strings["registered"])
+
 ####################
 #### Main stuff ####
 ####################
@@ -247,6 +312,7 @@ def main():
     dp.add_handler(CommandHandler("help", showHelp))
     dp.add_handler(CommandHandler("removeAll", removeAll))
     dp.add_handler(CommandHandler("purge", purge))
+    dp.add_handler(CommandHandler("register", register))
 
     dp.add_error_handler(error)
 
@@ -254,5 +320,5 @@ def main():
 
     updater.idle()
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
